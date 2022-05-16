b12065003b678593f479b0d3696b19b4806d94cf
Andy Z Wright <zhangruibin021@gmail.com>
2022-05-16 15:44:22 +0800

    fix DMPmodel

--- dl/seg/model/__init__.py
+++ dl/seg/model/__init__.py
@@ -6,9 +6,9 @@ from . import FcnResnet50
 
 
 class DPModels(enum.Enum):
-    fcn_resnet152: FcnResnet50.fcn_resnet152
-    fcn_resnet101: FcnResnet50.fcn_resnet101
-    fcn_resnet50: FcnResnet50.fcn_resnet50
+    fcn_resnet152 = FcnResnet50.fcn_resnet152
+    fcn_resnet101 = FcnResnet50.fcn_resnet101
+    fcn_resnet50 = FcnResnet50.fcn_resnet50
 
 
 if __name__ == '__main__':
