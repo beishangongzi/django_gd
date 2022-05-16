aff47dc88f5d4a7cf843b37746bf368a4d324219
Andy Z Wright <zhangruibin021@gmail.com>
2022-05-16 14:58:33 +0800

    change dataset

--- dl/seg/utils/split_obt.py
+++ dl/seg/utils/split_obt.py
@@ -26,14 +26,45 @@ for item in items:
     # mask = (mask != 0)
     # mask = np.stack([mask] * 32, axis=2)
     # X = mask * X
-    edge = [0, 128, 256, -256, -128, None]
-    for i in [0, 1, 3, 4]:
-        for j in [0, 1, 3, 4]:
-            y1 = Y[edge[i]:edge[i+1], edge[j]:edge[j+1], 0]
-            x1 = X[edge[i]:edge[i+1], edge[j]:edge[j+1], :]
-            name = item.split(".")[0] + "_{}_{}".format(i, j)
-            np.save(os.path.join(x_path, name), x1)
-            np.save(os.path.join(y_path, name), y1)
+    # edge = [0, 128, 256, -256, -128, None]
+    # for i in [0, 1, 3, 4]:
+    #     for j in [0, 1, 3, 4]:
+    #         y1 = Y[edge[i]:edge[i+1], edge[j]:edge[j+1], 0]
+    #         x1 = X[edge[i]:edge[i+1], edge[j]:edge[j+1], :]
+    #         name = item.split(".")[0] + "_{}_{}".format(i, j)
+    #         np.save(os.path.join(x_path, name), x1)
+    #         np.save(os.path.join(y_path, name), y1)
+
+
+    y1 = Y[0:256, 0:256, 0]
+    x1 = X[0:256, 0:256, :]
+    name = item.split(".")[0] + "_0"
+    np.save(os.path.join(x_path, name), x1)
+    np.save(os.path.join(y_path, name), y1)
+
+    y1 = Y[0:256, -256:, 0]
+    x1 = X[0:256, -256:, :]
+    name = item.split(".")[0] + "_1"
+    np.save(os.path.join(x_path, name), x1)
+    np.save(os.path.join(y_path, name), y1)
+
+    y1 = Y[-256:, 0:256, 0]
+    x1 = X[-256:, 0:256, :]
+    name = item.split(".")[0] + "_2"
+    np.save(os.path.join(x_path, name), x1)
+    np.save(os.path.join(y_path, name), y1)
+
+    y1 = Y[-256:, -256:, 0]
+    x1 = X[-256:, -256:, :]
+    name = item.split(".")[0] + "_3"
+    np.save(os.path.join(x_path, name), x1)
+    np.save(os.path.join(y_path, name), y1)
+    
+    y1 = Y[128:128+256, 128:128+256, 0]
+    x1 = X[128:128+256, 128:128+256, :]
+    name = item.split(".")[0] + "_4"
+    np.save(os.path.join(x_path, name), x1)
+    np.save(os.path.join(y_path, name), y1)
 
 if __name__ == '__main__':
     pass
