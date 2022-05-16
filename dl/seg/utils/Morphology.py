0bd688bfac01e2987eb317a90c8da83beac1057f
Andy Z Wright <zhangruibin021@gmail.com>
2022-05-16 16:20:14 +0800

    add code to test morphology

--- dl/seg/utils/Morphology.py
+++ dl/seg/utils/Morphology.py
@@ -31,3 +31,52 @@ class Morphology:
     @staticmethod
     def erode(img, kernel_size=3, iterations=1):
         return Morphology.fun(img, kernel_size, iterations, cv2.MORPH_ERODE)
+
+
+if __name__ == '__main__':
+    import torchvision.transforms as transforms
+    from PIL import Image
+    import numpy as np
+
+
+    def image_RGB2Gray(image_path = "./fusion_datasets/1.jpg"):
+        # 图片路径，相对路径
+
+        # 读取图片
+        image = Image.open(image_path)
+        # 输出维度
+        print("RGB图像的维度：", np.array(image).shape)
+        # 显示原图
+        image.show()
+        # RGB转换我灰度图像
+        image_transforms = transforms.Compose([
+            transforms.Grayscale(1)
+        ])
+        image = image_transforms(image)
+        # 输出灰度图像的维度
+        print("灰度图像维度： ", np.array(image).shape)
+        # 显示灰度图像
+        image.show()
+        return image
+
+
+    test_image = "img.png"
+    img = image_RGB2Gray(test_image)
+    img.show()
+    img = np.array(img)
+    img_close = Morphology.close(img, 5, 2)
+    img_close = Image.fromarray(img_close.cpu().numpy())
+    img_close.show("close")
+
+    img_close = Morphology.erode(img, 5, 2)
+    img_close = Image.fromarray(img_close.cpu().numpy())
+    img_close.show("erode")
+
+    img_close = Morphology.dilate(img, 5, 2)
+    img_close = Image.fromarray(img_close.cpu().numpy())
+    img_close.show("dilate")
+
+    img_close = Morphology.open(img, 5, 2)
+    img_close = Image.fromarray(img_close.cpu().numpy())
+    img_close.show("open")
+
